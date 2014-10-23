c32e3429cfe6a7b45ca152c72925728cadcf2123
wyllman <wyllman@gmail.com>
2014-10-23 19:28:42 +0100

    Adding tests of initialize method

--- lib/P5_T_34.rb
+++ lib/P5_T_34.rb
@@ -3,5 +3,9 @@ require "P5_T_34/version"
 module P5_T_34
   class Question
      attr_reader :text, :correct, :distractor
+
+     def initialize (textI, correctI, distractorI)
+  
+     end
   end
 end
--- spec/P5_T_34_spec.rb
+++ spec/P5_T_34_spec.rb
@@ -5,7 +5,7 @@ require 'P5_T_34'
 module P5_T_34
 	describe 'Question' do
 		before :each do
-      			@c = P5_T_34::Question.new
+      			@c = P5_T_34::Question.new("2+5=?","Ninguna es correcta", [1, 5, 6] )
     		end
 		context "Constructors test" do
 			it 'checking atributes' do
@@ -13,6 +13,12 @@ module P5_T_34
 				expect(@c).to respond_to(:correct)
 				expect(@c).to respond_to(:distractor)
         		end
+
+			it 'checking initialize' do
+				expect(@c.text).to eq("2+5=?")
+				expect(@c.correct).to eq("Ninguna es correcta")
+				expect(@c.distractor).to eq([1, 5, 6])
+			end
 		end
 	end
 end
