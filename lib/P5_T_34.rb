5c3566c726f8a4662783df2818b0bf6d3da6b4e8
wyllman <wyllman@gmail.com>
2014-10-30 11:23:17 +0000

    Create file for LList class

--- lib/P5_T_34.rb
+++ lib/P5_T_34.rb
@@ -1,6 +1,7 @@
 require "P5_T_34/version"
 require "P5_T_34/Question"
 require "P5_T_34/Exam"
+require "P5_T_34/LList"
 
 module P5_T_34
  
---
+++ lib/P5_T_34/LList.rb
@@ -0,0 +1,9 @@
+require "P5_T_34/version"
+
+
+module P5_T_34
+  class LList
+    #TODO
+  end
+  
+end
