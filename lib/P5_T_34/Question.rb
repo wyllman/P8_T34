2f03e0e9c10cb6252fbab31986ada0457737a09e
wyllman <wyllman@gmail.com>
2014-10-30 11:00:01 +0000

    Fixed internal structure.

---
+++ lib/P5_T_34/Question.rb
@@ -0,0 +1,26 @@
+require "P5_T_34/version"
+
+module P5_T_34
+   class Question
+     attr_reader :text, :correct, :distractor
+
+     def initialize (textI, correctI, distractorI)
+        @text = textI
+        @correct = correctI
+        @distractor = distractorI.map{|i| i.to_s}
+     end
+     
+     def questioning
+        return text
+     end
+
+     def answers
+        return (distractor + [correct]).shuffle
+     end
+     def to_s
+	result = text + "\n"
+	answers.each{|i| result += i + "\n"}
+	return result
+     end
+   end
+end
