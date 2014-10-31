c062ff77ff90f0cfdaf02dc2de742da21b7c1d84
alu0100611120 <alu0100611120@ull.edu.es>
2014-10-31 12:14:56 +0000

    Added comments

--- lib/P5_T_34/Exam.rb
+++ lib/P5_T_34/Exam.rb
@@ -2,14 +2,17 @@ require "P5_T_34/version"
 
 
 module P5_T_34
-  
+   # Define un tipo de LList específico para la creación de exámenes
+   # y los métodos necesarios para su manejo
    class Exam < LList
+     
      # Crea una lista cuyo primer nodo es el nombre ó
      # identificador del examen
      def initialize(nameI)
        super
      end
      
+     #Devuelve un examen entero, comvertido a string
      def to_s
        aux = "Título del examen: " + @top.value + "\n"
        aux += "------------------------------------------------- \n"
@@ -19,6 +22,7 @@ module P5_T_34
 	 aux += " \n ++++++++++++++++++++++++++++++++++ \n"
 	 act = act.next
        end
+       return aux
      end
    end
 end
