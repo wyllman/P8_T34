7b47be919d36e23e8d6467dcf4785995bea8458f
wyllman <wyllman@gmail.com>
2014-11-07 14:54:09 +0000

    Arreglos menores, dos nuevas expectativas QuestionVF y usar QuestionVF en Exam_spec.rb

--- spec/Exam_spec.rb
+++ spec/Exam_spec.rb
@@ -7,10 +7,10 @@ module P5_T_34
 	  before :each do
 	    @ex = Exam.new("Prueba 1")
 	    @q1 = Question.new("¿Cuál es la salida del siguiente código Ruby? \n class Xyz \n \t def pots \n \t\t @nice \n \t end \n end \n xyz = Xyz.new \n p xyz.pots","nil", ["#<Xyz:0x95d7718>", "0", "Ninguna de las anteriores"])
-	    @q2 = Question.new("¿Es válida la siguiente definición de un hash en Ruby? \n hash_raro = { \n\t [1,2,3] => Object.new(), \n\t Hash.new => :toto \n }", "Cierto", ["Falso"])
+	    @q2 = QuestionVF.new("¿Es válida la siguiente definición de un hash en Ruby? \n hash_raro = { \n\t [1,2,3] => Object.new(), \n\t Hash.new => :toto \n }", true)
 	    @q3 = Question.new("¿Cuál es la salida del siguiente código Ruby? \n class Array \n\t def say_hi \n\t\t \"HEY!\" \n\t end \n end \n p[1,\"bob\"].say_hi", "HEY!", ["1","bob","Ninguna de las anteriores"])
 	    @q4 = Question.new("¿Cuál es el tipo del objeto en el siguiente código Ruby? \n class Objeto \n end", "Una instancia de la clase Class", ["Una constante", "Un objeto", "Ninguna de las anteriores"])
-	    @q5 = Question.new("¿Es apropiado que una clase Tablero herede de una clase Juego?", "Falso", ["Verdadero"])
+	    @q5 = QuestionVF.new("¿Es apropiado que una clase Tablero herede de una clase Juego?", false)
 	    @ex.push(@q1,@q2,@q3,@q4,@q5)
 	  end
 	  context "Class Exam test" do
--- spec/QuestionVF_spec.rb
+++ spec/QuestionVF_spec.rb
@@ -5,16 +5,21 @@ require 'P5_T_34'
 module P5_T_34
 	describe 'QuestionVF' do
 	  before :each do
-	    @q = QuestionVF.new("¿Es verdadero?", true)
+	    @q = QuestionVF.new("¿Es verdadero?", true) 
+	    
 	  end
 	  context 'Constructor test' do
 	    it "Checking initialize" do
-	      expect(QuestionVF).to respond_to(:new)
+	        expect(QuestionVF).to respond_to(:new)
+	        expect(@q).to be_a(QuestionVF)
+	        expect(@q).to be_a(Question)
+	        expect(@q).not_to be_an_instance_of(Question)
+	        expect(@q).to be_an_instance_of(QuestionVF)
 	    end
 	    it "Checking atributes" do
 	        expect(@q).to respond_to(:text)
-		expect(@q).to respond_to(:correct)
-		expect(@q).to respond_to(:distractor)
+		    expect(@q).to respond_to(:correct)
+		    expect(@q).to respond_to(:distractor)
 	    end
 	  end
 	end
