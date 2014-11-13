b516801a69ed35ea5c3c8b2252a40698c4325b69
wyllman <wyllman@gmail.com>
2014-11-13 14:46:52 +0000

    Comparable specs

--- spec/Question_spec.rb
+++ spec/Question_spec.rb
@@ -22,7 +22,7 @@ module P5_T_34
 		end
 
 		context "Methods test" do
-                        it 'checking existing methods' do
+            it 'checking existing methods' do
 				expect(@q).to respond_to(:questioning)
 				expect(@q).to respond_to(:answers)
 				expect(@q).to respond_to(:to_s)
@@ -35,6 +35,18 @@ module P5_T_34
 				expect(@q.to_s).to match( /^(2\+5=\?)\n(.+)\n(.+)\n(.+)\n(.+)/ )				
 			end
 
+		end
+		
+		context "Comparable methods" do
+			it 'Basic methods' do
+		 		q2 = P5_T_34::Question.new("201+205=?","Ninguna es correcta", [22, 311, 0] )
+		 		#expect(@q).to respond_to(==)
+		 		expect(@q == q2).to eq(false)
+		 		expect(@q < q2).to eq(true)
+		 		expect(@q > q2).to eq(false)
+		 		expect(@q <= q2).to eq(true)
+		 		expect(@q >= q2).to eq(false)
+		 	end
 		end
 	end
 end
