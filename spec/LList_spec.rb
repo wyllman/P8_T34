bbfa80f9602e42e0d52f2e694922ba4ea5d7c03f
wyllman <wyllman@gmail.com>
2014-11-13 16:00:15 +0000

    Primera expectativa para hacer enumerable la clase Llist

--- spec/LList_spec.rb
+++ spec/LList_spec.rb
@@ -37,42 +37,50 @@ module P5_T_34
 	       
 	     end
 	     
-	     it 'checking class LList initialize' do
-	       expect(@l.top).to eq(@n)
-	       # Modf
-	       expect(@l.tail).to eq(@n)
-	     end
+	     	it 'checking class LList initialize' do
+	       		expect(@l.top).to eq(@n)
+	       		# Modf
+	       		expect(@l.tail).to eq(@n)
+	     	end
 	     
-	     it 'Checking class LList push method' do 
-               expect(@l).to respond_to(:push)
-	       expect(@l.push(7)).to eq(2)
-	       expect(@l.push(8)).to eq(3)
-	       expect(@l.push(9, 10, 11)).to eq(6)
-	       # Modf
-	       expect(@l.top.prev).to eq(nil)
-	       expect(@l.tail.prev.value).to eq(10)
-	     end
+			it 'Checking class LList push method' do 
+               	expect(@l).to respond_to(:push)
+	       		expect(@l.push(7)).to eq(2)
+	       		expect(@l.push(8)).to eq(3)
+	       		expect(@l.push(9, 10, 11)).to eq(6)
+	       		# Modf
+	       		expect(@l.top.prev).to eq(nil)
+	       		expect(@l.tail.prev.value).to eq(10)
+			end
 
-             it 'Checking class LList to_node method' do
+			it 'Checking class LList to_node method' do
                expect(LList).to respond_to(:to_node)
                expect(LList.to_node(90)).to be_a(Node) 
                expect(LList.to_node(90).value).to eq(90)
-	       expect(LList.to_node(90).next).to eq(nil)
-	       expect(LList.to_node(90).prev).to eq(nil)
-             end  	     
+			   expect(LList.to_node(90).next).to eq(nil)
+			   expect(LList.to_node(90).prev).to eq(nil)
+			end  	     
 	     
-             it 'Checking class LList pop method' do
+        	it 'Checking class LList pop method' do
                expect(@l).to respond_to(:pop)
-	       # Modf
-	       @l.push(7)
-	       expect(@l.pop).to eq(@n)
-	       # Modf
-	       expect(@l.top.prev).to eq(nil)
-             end
+               # Modf
+               @l.push(7)
+               expect(@l.pop).to eq(@n)
+               # Modf
+               expect(@l.top.prev).to eq(nil)
+        	end
 
-             it 'Checking class LList [] operator' do
+        	it 'Checking class LList [] operator' do
                expect(@l).to respond_to(:[])
-             end
-           end
+        	end
+ 		end
+           
+       	context "Enumerable methods" do
+			it 'Basic methods' do
+		 	#respond_to
+		 	expect(@l).to respond_to(:each)
+		 	
+		 	end
+		end
 	end
 end
