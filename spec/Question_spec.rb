98ffd1596ee0738c2373e6b9d29d8fd09f9459c6
alu0100611120 <cabaleiro8@gmail.com>
2014-11-13 15:47:40 +0000

    Retocando comparable y espectativas2

--- spec/Question_spec.rb
+++ spec/Question_spec.rb
@@ -40,12 +40,14 @@ module P5_T_34
 			it 'Basic methods' do
 		 		q2 = P5_T_34::Question.new("201+205=?","Ninguna es correcta", [22, 311, 0] )
 		 		q3 = P5_T_34::Question.new("201+205=?","Ninguna es correcta", [22, 311, 0] )
+		 		#Respon_to
 		 		expect(@q).to respond_to(:==)
 		 		expect(@q).to respond_to(:>)
 		 		expect(@q).to respond_to(:<)
 		 		expect(@q).to respond_to(:>=)
 		 		expect(@q).to respond_to(:<=)
 		 		expect(@q).to respond_to(:between?)
+		 		#Pruebas
 		 		expect(@q == q2).to eq(false)
 		 		expect(q2 == q3).to eq(true)
 		 		expect(@q < q2).to eq(true)
